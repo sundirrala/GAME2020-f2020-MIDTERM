2bd1406be22ddca55b509ecef6d6f0955be53f6f
sundirrala <nisara.drahman@gmail.com>
2020-10-21 06:11:28 -0400

    bullet movement and enemy movement

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -9,12 +9,12 @@ public class PlayerController : MonoBehaviour
     public BulletManager bulletManager;
 
     [Header("Boundary Check")]
-    public float horizontalBoundary;
+    public float verticalBoundary;
 
     [Header("Player Speed")]
-    public float horizontalSpeed;
+    public float verticalSpeed;
     public float maxSpeed;
-    public float horizontalTValue;
+    public float verticalTValue;
 
     [Header("Bullet Firing")]
     public float fireDelay;
@@ -56,13 +56,13 @@ public class PlayerController : MonoBehaviour
         {
             var worldTouch = Camera.main.ScreenToWorldPoint(touch.position);
 
-            if (worldTouch.x > transform.position.x)
+            if (worldTouch.y > transform.position.y)
             {
                 // direction is positive
                 direction = 1.0f;
             }
 
-            if (worldTouch.x < transform.position.x)
+            if (worldTouch.y < transform.position.y)
             {
                 // direction is negative
                 direction = -1.0f;
@@ -72,26 +72,13 @@ public class PlayerController : MonoBehaviour
 
         }
 
-        // keyboard support
-        if (Input.GetAxis("Horizontal") >= 0.1f) 
+        if (m_touchesEnded.y != 0.0f)
         {
-            // direction is positive
-            direction = 1.0f;
-        }
-
-        if (Input.GetAxis("Horizontal") <= -0.1f)
-        {
-            // direction is negative
-            direction = -1.0f;
-        }
-
-        if (m_touchesEnded.x != 0.0f)
-        {
-           transform.position = new Vector2(Mathf.Lerp(transform.position.x, m_touchesEnded.x, horizontalTValue), transform.position.y);
+           transform.position = new Vector2(Mathf.Lerp(transform.position.y, m_touchesEnded.y, verticalTValue), transform.position.x);
         }
         else
         {
-            Vector2 newVelocity = m_rigidBody.velocity + new Vector2(direction * horizontalSpeed, 0.0f);
+            Vector2 newVelocity = m_rigidBody.velocity + new Vector2(verticalSpeed * direction, 0.0f);
             m_rigidBody.velocity = Vector2.ClampMagnitude(newVelocity, maxSpeed);
             m_rigidBody.velocity *= 0.99f;
         }
@@ -100,15 +87,15 @@ public class PlayerController : MonoBehaviour
     private void _CheckBounds()
     {
         // check right bounds
-        if (transform.position.x >= horizontalBoundary)
+        if (transform.position.y >= verticalBoundary)
         {
-            transform.position = new Vector3(horizontalBoundary, transform.position.y, 0.0f);
+            transform.position = new Vector3(transform.position.x, verticalBoundary, 0.0f);
         }
 
         // check left bounds
-        if (transform.position.x <= -horizontalBoundary)
+        if (transform.position.y <= -verticalBoundary)
         {
-            transform.position = new Vector3(-horizontalBoundary, transform.position.y, 0.0f);
+            transform.position = new Vector3( transform.position.x, -verticalBoundary, 0.0f);
         }
 
     }
