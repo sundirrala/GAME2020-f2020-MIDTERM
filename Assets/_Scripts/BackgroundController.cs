e18919b73c9d8a404e60dd436ff44ed8c897d20c
sundirrala <nisara.drahman@gmail.com>
2020-10-21 03:49:49 -0400

    background scroll on the x

--- Assets/_Scripts/BackgroundController.cs
+++ Assets/_Scripts/BackgroundController.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// this is for the scrolling background. it resets when the second background reaches the boundary.
+/// Date Modified: 2020-10-21
+/// </summary>
+
 public class BackgroundController : MonoBehaviour
 {
     public float verticalSpeed;
@@ -16,18 +21,18 @@ public class BackgroundController : MonoBehaviour
 
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3(verticalBoundary, 0.0f); // you'll need to flip this, since we're more concerned about the x bound than the y.
     }
 
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(verticalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= -verticalBoundary)
         {
             _Reset();
         }
